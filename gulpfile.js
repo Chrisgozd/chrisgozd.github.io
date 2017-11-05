f17fd2dba82adad58c4872834425c3c9832539bf
Christopher Gozdziewski <Chrisgozd92@gmail.com>
2017-11-05 19:46:46 +0000

    change CNAME

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp'),
-	plumber = require('gulp-plumber'),
-	uglify = require('gulp-uglify'),
-	sass = require('gulp-sass'),
-	rename = require('gulp-rename'),
-	minifyCSS = require('gulp-minify-css'),
-	autoprefixer = require('gulp-autoprefixer');
+    plumber = require('gulp-plumber'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('scripts', function() {
+gulp.task('default', () =>
+    gulp.src('images/*')
+    .pipe(imagemin([], {
+        verbose: true
+    }))
+    .pipe(gulp.dest('images/dist'))
+);
+
+gulp.task('scripts', function () {
     return gulp.src('js/scripts.js')
         .pipe(plumber(plumber({
             errorHandler: function (err) {
@@ -15,18 +24,22 @@ gulp.task('scripts', function() {
             }
         })))
         .pipe(uglify())
-        .pipe(rename({extname: '.min.js'}))
+        .pipe(rename({
+            extname: '.min.js'
+        }))
         .pipe(gulp.dest('js'));
 });
 
 gulp.task('styles', function () {
     return gulp.src('./scss/styles.scss')
         .pipe(sass().on('error', sass.logError))
-		.pipe(minifyCSS({ keepSpecialComments: 0 }))
+        .pipe(minifyCSS({
+            keepSpecialComments: 0
+        }))
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', ['scripts', 'styles'], function() {
+gulp.task('watch', ['scripts', 'styles'], function () {
     gulp.watch('js/*.js', ['scripts']);
     gulp.watch('scss/*.scss', ['styles']);
 });
